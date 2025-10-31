5014f5a20c91e5acfb1e554c0e7b1ceaca06b2c9
mangkidals <kenzoghaisanimudz@gmail.com>
2025-10-31 19:45:10 +0700

    fix-deploy-vercel-error

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -4,7 +4,7 @@ import supabase  from '../supabase-client'
 const LoginPage = () => {
     
     const handlegoogleLogin = async () => {
-        const { data, error } = await supabase.auth.signInWithOAuth({
+        const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
                 redirectTo: `${window.location.origin}/home`,
