92f9125f3860f45a94755c97e41c6d11b0634064
mangkidals <kenzoghaisanimudz@gmail.com>
2025-11-20 16:06:12 +0700

    fix add remember auth

--- src/custom-components/Navbar.tsx
+++ src/custom-components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 import { Home, LogOut } from 'lucide-react';
 import { Link, useNavigate } from 'react-router';
